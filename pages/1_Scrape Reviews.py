d2c552fcd64d03061d492511ee64fbe81a47aa16
Chad <75446966+chadd28@users.noreply.github.com>
2024-02-15 21:08:31 -0600

    Add files via upload

--- pages/1_Scrape Reviews.py
+++ pages/1_Scrape Reviews.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
+import requests
+from bs4 import BeautifulSoup
 import numpy as np
+import time
 
 st.title('Scrape data')
 
@@ -10,24 +13,24 @@ if st.button("Scrape Reviews"):
     with st.status("Performing Webscrape...", expanded=True) as status:
         st.write("Scraping from + " + url + "...")
         # collecting the data
-        # root = url
-        # r = requests.get(root)
-        # print(r.status_code)
-        # soup=BeautifulSoup(r.text, 'html.parser')
-        # divs = soup.findAll(class_="comment__09f24__D0cxf css-qgunke")
-        # reviews = []
-        # for div in divs:
-        #     reviews.append(div.find('span').text)
+        root = url
+        r = requests.get(root)
+        print(r.status_code)
+        soup=BeautifulSoup(r.text, 'html.parser')
+        divs = soup.findAll(class_="comment__09f24__D0cxf css-qgunke")
+        reviews = []
+        for div in divs:
+            reviews.append(div.find('span').text)
 
         # looping through pages of yelp restaurant to scrape each review
-        # for page in range (10, 80, 10):
-        #     r = requests.get(root+'?start='+str(page))
-        #     soup=BeautifulSoup(r.text, 'html.parser')
-        #     divs = soup.findAll(class_="comment__09f24__D0cxf css-qgunke")
-        #     for div in divs:
-        #         reviews.append(div.find('span').text)
-        df = pd.read_csv('reviews.csv')         # used for testing when requests are overloaded
-        reviews = df['Review'].tolist()
+        for page in range (10, 80, 10):
+            r = requests.get(root+'?start='+str(page))
+            soup=BeautifulSoup(r.text, 'html.parser')
+            divs = soup.findAll(class_="comment__09f24__D0cxf css-qgunke")
+            for div in divs:
+                reviews.append(div.find('span').text)
+        # df = pd.read_csv('reviews.csv')         # used for testing when requests are overloaded
+        # reviews = df['Review'].tolist()         # used for testing when requests are overloaded
         
         df = pd.DataFrame(np.array(reviews), columns=['review'])
 
